566f3ed56b4ca090ceead6dd75aac7748b926908
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-02-17 17:00:06 -0300

    Create unix_commands.py

---
+++ unix_commands.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.6
+
+#Simple example of how to use Unix commands from within a python script.
+#Exemplos simples de como executar comandos Unix a partir de script em python.
+import subprocess
+
+subprocess.call(["uname", "-r"])
+#This code gives the same output as the command "uname -r", it should print the version of the kernel.
+#Este codigo executa o comando "uname -r", este apresenta a versao do kernel em uso.
