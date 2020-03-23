8618d10220826b9491d47aa813e35c9d6b21719b
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-03-23 17:35:26 -0300

    Update mysql_connector.py

--- mysql_connector.py
+++ mysql_connector.py
@@ -2,6 +2,8 @@ import mysql.connector
 
 class mysql_helper:
 
+    # connect function, to be used every time the database will be acessed
+    # função de conexão a database, deve ser usada sempre que a database será acessada
     def db_connect(self):
         connection = mysql.connector.connect(
             host="localhost",
