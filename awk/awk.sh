bf9ef377c7ee7e26945e684d671acc4244dd65f2
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-10-05 20:49:40 -0300

    Create awk.sh

---
+++ awk/awk.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+#use awk to print only certain columns from one file to another
+#usando awk para passar apenas algumas colunas de um arquivo para outro
+
+awk '{print $2 $4}' in.txt out.txt
