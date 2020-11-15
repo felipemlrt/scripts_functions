b51746f2acfdee478ef8b14eb89b3caacd5abe41
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-11-15 15:50:07 -0300

    Update run_for_x_seconds.py

--- run_for_x_seconds.py
+++ run_for_x_seconds.py
@@ -5,5 +5,5 @@ end_time = t.time() + 60
 while t.time() < end_time:
     print ("not yet done")
 print ("done")
-#Be carefull, the time counted by the system in this manner is not precise and should not be trsted for critical time keeping
-#
+#Be carefull, the time counted by the system in this manner is not precise and should not be trsted for critical time keeping.
+#Cuidado a contagem de tempo desta forma não é precisa e não deve ser usada para situações onde a temporização é crítica.
