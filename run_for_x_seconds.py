90896170fc5a7a0407cfd26995cf313b937d12bf
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-01-03 19:02:57 -0300

    Update run_for_x_seconds.py

--- run_for_x_seconds.py
+++ run_for_x_seconds.py
@@ -1,5 +1,6 @@
 import time as t
-# quick easy way to create a loop that runs for a fixed time
+# quick, easy, not very precise way to create a loop that runs for a fixed time
+# forma rapida, simples e nao muito precisa de criar um loop que roda por um tempo definido
 # + 60 = runs 1 minute, + 60 * 5 = 5 minutes, + 60 * 30 = 30 minutes, + 10 = 10 seconds, and so on
 end_time = t.time() + 60
 while t.time() < end_time:
