ca84a92e5b0634ab9c6b95f1a9121ffe67995e14
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-01-31 19:22:44 -0300

    Update check_previous_elements_list.py

--- python/check_previous_elements_list.py
+++ python/check_previous_elements_list.py
@@ -1,6 +1,12 @@
 #quick way to check each element from a series against part of it
 test = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-if all(10 x for x in test):
+if all(10 <= x for x in test):
  print("test 1")
-if all(x == 0 for x in (test[0:5)):
+if all(x == 0 for x in test[0:5]):
  print("test 2")
+
+#to check all in a list against all from anopther list
+testb = [0,10,25,50,80]
+for i in range(0, len(testb)):
+ if all(testb[i] >= x for x in test):
+  print("test 3")
